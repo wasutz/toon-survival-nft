7bed654c5a859a6ef680c7d286579356dde447e7
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2022-03-12 22:00:44 +0700

    copy contract from toon survival truffle and made it works with hardhat

---
+++ contracts/ToonSurvival.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ToonSurvival is ERC721A, Ownable {
+  using Strings for uint256;
+
+  enum Stages {
+    Paused,
+    Presale,
+    PublicSale
+  }
+
+  string baseURI;
+  string public hiddenBaseURI;
+  uint256 public cost = 0.1 ether;
+  uint256 public maxSupply = 100;
+  uint256 public maxMintAmount = 5;
+  uint256 public maxMintAmountPerTx = 2;
+  bool public revealed = false;
+  Stages public stage = Stages.Paused;
+
+  mapping(address => bool) private whitelist;
+
+  constructor(
+    string memory _initBaseURI,
+    string memory _initHiddenBaseURI
+  ) ERC721A("Toon Survival", "TSVR") {
+    baseURI = _initBaseURI;
+    hiddenBaseURI = _initHiddenBaseURI;
+  }
+
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(totalSupply() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
+  }
+
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+    require(stage != Stages.Paused, "The contract is paused!");
+    require(balanceOf(msg.sender) + _mintAmount <= maxMintAmount, "Mint over max mint amount!");
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+
+    if (stage == Stages.Presale) {
+      require(isWhitelist(msg.sender), "User is not whitelisted!");
+    }
+
+    _safeMint(msg.sender, _mintAmount);
+  }
+
+  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+    _safeMint(_receiver, _mintAmount);
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    string memory currentBaseURI = revealed ? _baseURI() : hiddenBaseURI;
+
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString()))
+        : "";
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setHiddenBaseURI(string memory _newHiddenBaseURI) public onlyOwner {
+    hiddenBaseURI = _newHiddenBaseURI;
+  }
+
+  function setCost(uint256 _cost) public onlyOwner() {
+    cost = _cost;
+  }
+
+  function setMaxMintAmount(uint256 _maxMintAmount) public onlyOwner() {
+    maxMintAmount = _maxMintAmount;
+  }
+
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner() {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+  }
+
+  function setStage(Stages _stage) public onlyOwner {
+    stage = _stage;
+  }
+
+  function isWhitelist(address _address) public view returns (bool){
+    return whitelist[_address];
+  }
+
+  function setRevealed(bool _revealed) public onlyOwner {
+    revealed = _revealed;
+  }
+
+  function withdraw() public payable onlyOwner {
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(success);
+  }
+
+  function addToWhitelist(address[] calldata _addresses) external onlyOwner {
+    for (uint256 i; i < _addresses.length; i++) {
+        whitelist[_addresses[i]] = true;
+    }
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+}
---
+++ test/ToonSurvivalTest.js
@@ -0,0 +1,152 @@
+const chai = require('chai')
+const ChaiAsPromised = require('chai-as-promised');
+const {ethers} = require('hardhat');
+const web3 = require('web3');
+const {expect} = chai;
+
+chai.use(ChaiAsPromised);
+
+describe("ToonSurvival", () => {
+    let toonSurvival = null;
+    let addr1, addr2, addr3;
+    const baseUri = "ipfs://baseUri/";
+    const hiddenBaseUri = "ipfs://hiddenBaseUri/";
+    const stages = {
+        Paused: 0,
+        Presale: 1,
+        PublicSale: 2
+    }
+
+    beforeEach(async () => {
+        [addr1, addr2, addr3] = await ethers.getSigners();
+
+        const ToonSurvivalContract = await ethers.getContractFactory("ToonSurvival");
+        toonSurvival = await ToonSurvivalContract.deploy(baseUri, hiddenBaseUri);
+
+        await toonSurvival.deployed();
+    });
+
+    it('should has 0 totalSupply with no minted token', async () => {
+        const supply = await toonSurvival.totalSupply();
+
+        expect(supply.toNumber()).to.equal(0);
+    });
+
+    it('should on Stage.Paused by default', async () => {
+        const stage = await toonSurvival.stage();
+
+        expect(stage).to.equal(stages.Paused);
+    });
+
+    it('should mint failed when mint amount is 0', async () => {
+        await expect(toonSurvival.mint(0)).to.be.revertedWith('Invalid mint amount!');
+    });
+
+    it('should mint failed when mintAmount is greater than maxMintAmountPerTx', async () => {
+        await expect(toonSurvival.mint(6, {
+            value: web3.utils.toWei('0.1', 'ether')
+        })).to.be.revertedWith('Invalid mint amount!');
+    });
+
+    it('should mint failed when over max supply', async () => {
+        await toonSurvival.setStage(stages.PublicSale);
+        await toonSurvival.setMaxMintAmount(100);
+        await toonSurvival.setMaxMintAmountPerTx(100);
+        await toonSurvival.mint(100, {
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        await expect(toonSurvival.mint(1, {
+            value:  web3.utils.toWei('0.1', 'ether')
+        })).to.be.revertedWith('Max supply exceeded!');
+    });
+
+    it('should mint failed when address mint amount is greater than maxMintAmount', async () => {
+        await toonSurvival.setStage(stages.PublicSale);
+        await toonSurvival.mint(2, {value: web3.utils.toWei('0.2', 'ether')});
+        await toonSurvival.mint(2, {value: web3.utils.toWei('0.2', 'ether')});
+        await toonSurvival.mint(1, {value: web3.utils.toWei('0.1', 'ether')});
+
+        await expect(toonSurvival.mint(1, {
+            from: await addr1.getAddress(),
+            value: web3.utils.toWei('0.1', 'ether')
+        })).to.be.revertedWith('Mint over max mint amount!');
+    });
+
+    it('should mint failed when insufficient fund', async () => {
+        await toonSurvival.setStage(stages.PublicSale);
+        await expect(toonSurvival.connect(addr3).mint(1))
+            .to.be.rejectedWith(Error, 'insufficient funds for intrinsic transaction cost');
+    });
+
+    it('should mint failed when contract is Paused', async () => {
+        await expect(toonSurvival.mint(1, {
+            value: web3.utils.toWei('0.1', 'ether')
+        })).to.be.revertedWith("The contract is paused!");
+    });
+
+    it('should be able mintForAddress even the contract is Paused', async () => {
+        await toonSurvival.mintForAddress(1, await addr2.getAddress());
+
+        const supply = await toonSurvival.totalSupply();
+
+        expect(supply.toNumber()).to.equal(1);
+    });
+
+    it('should mint failed when on Presale stage and the user doese not in whitelisted', async () => {
+        await toonSurvival.setStage(stages.Presale);
+        await expect(toonSurvival.connect(addr2).mint(1, {
+            value: web3.utils.toWei('0.1', 'ether')
+        })).to.be.revertedWith("User is not whitelisted!");
+    });
+
+    it('should have token when mint on Presale and the user is in whitelisted', async () => {
+        await toonSurvival.addToWhitelist([await addr2.getAddress()]);
+        await toonSurvival.setStage(stages.Presale);
+        await toonSurvival.connect(addr2).mint(1, {
+            value: web3.utils.toWei('0.1', 'ether')
+        });
+
+        const supply = await toonSurvival.totalSupply();
+        const isWhitelist = await toonSurvival.isWhitelist(await addr2.getAddress());
+
+        expect(isWhitelist).to.equal(true);
+        expect(supply.toNumber()).to.equal(1);
+    });
+
+    it('should have token when mint on public sale success', async () => {
+        await toonSurvival.setStage(stages.PublicSale);
+        await toonSurvival.connect(addr3).mint(2, {
+            value: web3.utils.toWei('0.2', 'ether')
+        });
+
+        const supply = await toonSurvival.totalSupply();
+        const isWhitelist = await toonSurvival.isWhitelist(await addr3.getAddress());
+
+        expect(isWhitelist).to.equal(false);
+        expect(supply.toNumber()).to.equal(2);
+    });
+
+    it('should get hiddenBaseURI with token id when get tokenURI and revealed is false', async () => {
+        await toonSurvival.setStage(stages.PublicSale);
+        await toonSurvival.connect(addr1).mint(2, {
+            value: web3.utils.toWei('0.2', 'ether')
+        });
+
+        const tokenUri = await toonSurvival.tokenURI(1);
+
+        expect(tokenUri).to.equal(hiddenBaseUri + "1");
+    });
+
+    it('should get baseUri with token id when get tokenURI and revealed is true', async () => {
+        await toonSurvival.setStage(stages.PublicSale);
+        await toonSurvival.setRevealed(true);
+        await toonSurvival.connect(addr1).mint(2, {
+            value: web3.utils.toWei('0.2', 'ether')
+        });
+
+        const tokenUri = await toonSurvival.tokenURI(1);
+
+        expect(tokenUri).to.equal(baseUri + "1");
+    });
+});
